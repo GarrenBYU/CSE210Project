5bf096c77d330dca23e03205a339da99134798a7
GarrenBYU <mei21004@byui.edu>
2023-09-14 03:51:35 -0600

    This has the core challenges to the 4th prep

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Globalization;
+using System.Xml.XPath;
+using System.Collections.Generic
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of number, type 0 when finished.");
+        String result = "";
+        List<int> numbers = new List<int>();
+        while (result != "0")
+        {
+            Console.Write("Enter number: ");
+            result = Console.ReadLine();
+            numbers.Add(int.Parse(result));
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        int listLength = numbers.Count;
+        int average = sum / listLength;
+        Console.WriteLine($"The average is: {average}")
     }
 }

1908fc0bbfa85875e7237dee34cc707b3cc3a0c5
GarrenBYU <mei21004@byui.edu>
2023-09-14 03:13:39 -0600

    Here are the core requirements for the 2nd prep activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        String userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        String letter;
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A!");
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (grade >= 70){
+            Console.WriteLine($"Congrats on passing your class with a {letter}!");
+        }
+        else
+        {
+            Console.WriteLine($"You did not pass this time around and got an F. Next time try to study.");
+        }
     }
 }

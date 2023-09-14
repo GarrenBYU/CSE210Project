47c74512548da2e94af2dfeba17845985c77ee02
GarrenBYU <mei21004@byui.edu>
2023-09-14 03:22:13 -0600

    First step for the 3rd prep activity.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int initialNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        if (initialNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (initialNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }

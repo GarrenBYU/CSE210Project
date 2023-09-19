adcaf27a8beacd6ef19e7823923e46727541e69e
GarrenBYU <mei21004@byui.edu>
2023-09-19 02:46:46 -0600

    Initial commit with the Job document and the first two jobs made in Main()

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,7 @@
+public class Job
+{
+    public string __company;
+    public string __jobTitle;
+    public int __startYear;
+    public int __endYear;
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1.__jobTitle = "Software Engineer";
+        job1.__company = "Microsoft";
+        job1.__startYear = 2023;
+        job1.__endYear = 2025;
+        
+        Job job2 = new Job();
+
+        job2.__jobTitle = "QA Engineer";
+        job2.__company = "Apple";
+        job2.__startYear = 2023;
+        job2.__endYear = 2025;
+        Console.WriteLine(job1.__company);
+        Console.WriteLine(job2.__company);
     }
 }

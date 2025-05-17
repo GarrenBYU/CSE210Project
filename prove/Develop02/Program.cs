291ab135fb7e76b1958b8cb23572be8bd366df15
GarrenBYU <mei21004@byui.edu>
2025-05-17 17:18:24 -0600

    New updates to the following

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -22,17 +22,14 @@ class Program
         Console.WriteLine("Welcome to the Journal Program!");
         var choice = MenuItem.Save;
         while(choice != MenuItem.Quit){
-            Console.WriteLine("Hello {0} {0} how are you", "Garren");
+            Console.WriteLine("Hello {0} how are you?", "Garren");
             Console.WriteLine($"");
-            Console.WriteLine(@"Hello Clay C:\temp");
-            Console.WriteLine("""
-Hello world "H4llo clay"
-Next Line
-Part3
-""");
+
             Console.Write("Please select one of the following choices: \n 1. Write \n 2. Display \n 3. Load \n 4. Save \n 5. Quit \nWhat would you like to do? ");
             choice = (MenuItem) int.Parse(Console.ReadLine());
             var fileName = "";
+            var passChoice = "";
+            var pass = "";
             switch (choice)
             {
                 case MenuItem.Write:
@@ -40,16 +37,25 @@ Part3
                     break;
                 case MenuItem.Display:
                     newJournal.DisplayAll();
+                    Console.Write("Please hit enter when you are done.");
+                    Console.ReadLine();
                     break;
                 case MenuItem.Load:
                     Console.Write("What is the file name? ");
                     fileName = Console.ReadLine();
+                    //newJournal.RecievePassword(fileName);
                     newJournal.LoadFromFile(fileName);
                     break;
                 case MenuItem.Save:
                     Console.Write("What is the file name? ");
                     fileName = Console.ReadLine();
-                    newJournal.SaveToFile(fileName);
+                    Console.Write("Do you want a password? ");
+                    passChoice = Console.ReadLine();
+                    if(passChoice == "Yes" || passChoice == "yes"){
+                        Console.Write("What is the password? ");
+                        pass = Console.ReadLine();
+                    }
+                    newJournal.SaveToFile(fileName, pass);
                     break;
                 case MenuItem.Quit:
                     break;
